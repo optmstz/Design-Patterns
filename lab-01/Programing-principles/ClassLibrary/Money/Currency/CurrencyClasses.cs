b555c1a424dd2703c8b2b12fff236a8f362c571d
Viktor Yelenetskyi <ipz223_yevv@student.ztu.edu.ua>
2024-04-01 19:32:36 +0300

    New ICurrency interface, added three classes Dollar, Euro and  Hryvnas

---
+++ lab-01/Programing-principles/ClassLibrary/Money/Currency/CurrencyClasses.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.MoneyPart.Currency
+{
+    public class GrivnaMoney : Money, ICurrency
+    {
+        public string currency { get; set; } = "UAH";
+
+        public GrivnaMoney(int wholePart, int fractionalPart) : base(wholePart, fractionalPart)
+        {
+
+        }
+        public override string DisplayAmount()
+        {
+            return $"{wholePart}.{fractionalPart} {currency}";
+        }
+    }
+
+    public class DollarMoney : Money, ICurrency
+    {
+        public string currency { get; set; } = "USD";
+
+        public DollarMoney(int wholePart, int fractionalPart) : base(wholePart, fractionalPart)
+        {
+
+        }
+        public override string DisplayAmount()
+        {
+            return $"{wholePart}.{fractionalPart} {currency}";
+        }
+    }
+
+    public class EuroMoney : Money, ICurrency
+    {
+        public string currency { get; set; } = "EUR";
+
+        public EuroMoney(int wholePart, int fractionalPart) : base(wholePart, fractionalPart)
+        {
+
+        }
+        public override string DisplayAmount()
+        {
+            return $"{wholePart}.{fractionalPart} {currency}";
+        }
+    }
+
+    public class NoCurrencyMoney : Money
+    {
+        public NoCurrencyMoney(int wholePart, int fractionalPart) : base(wholePart, fractionalPart)
+        {
+
+        }
+    }
+}
---
+++ lab-01/Programing-principles/ClassLibrary/Money/Currency/ICurrency.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.MoneyPart.Currency
+{
+    public interface ICurrency
+    {
+        string currency { get; set; }
+    }
+}
+

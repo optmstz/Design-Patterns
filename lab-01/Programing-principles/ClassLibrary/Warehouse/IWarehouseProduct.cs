f29b1af35e75b3d7e429ad94aeec064ee3aac4fd
Viktor Yelenetskyi <ipz223_yevv@student.ztu.edu.ua>
2024-04-01 19:35:52 +0300

    IWarehouseProduct interface expands existing Product, created WarehouseProduct class

---
+++ lab-01/Programing-principles/ClassLibrary/Warehouse/IWarehouseProduct.cs
@@ -0,0 +1,15 @@
+﻿using ClassLibrary.ProductPart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.WarehousePart
+{
+    public interface IWarehouseProduct : IProduct
+    {
+        int quantity { get; set; }
+        DateTime lastStockedDate { get; set; }
+    }
+}
---
+++ lab-01/Programing-principles/ClassLibrary/Warehouse/WarehouseProduct.cs
@@ -0,0 +1,28 @@
+﻿using ClassLibrary.MoneyPart;
+using ClassLibrary.ProductPart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.WarehousePart
+{
+    public class WarehouseProduct: IWarehouseProduct
+    {
+        public string name { get; protected set; } 
+        public string category { get; protected set; }
+        public Money price { get; set; }
+        public int quantity { get; set; } 
+        public DateTime lastStockedDate { get; set; }
+
+        public WarehouseProduct(string name, string category, Money price, int quantity, DateTime lastStockedDate)
+        {
+            this.name = name;
+            this.category = category;
+            this.price = price;
+            this.quantity = quantity;
+            this.lastStockedDate = lastStockedDate;
+        }
+    }
+}

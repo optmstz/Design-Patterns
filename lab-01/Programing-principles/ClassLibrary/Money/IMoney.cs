def78fcb73dc57dfa937773bd802f66660465e4a
Viktor Yelenetskyi <ipz223_yevv@student.ztu.edu.ua>
2024-04-01 19:30:23 +0300

    Created IMoney interface

---
+++ lab-01/Programing-principles/ClassLibrary/Money/IMoney.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.MoneyPart
+{
+    public interface IMoney
+    {
+        int wholePart { get; set; }
+        int fractionalPart { get; set; }
+
+        string DisplayAmount();
+    }
+}

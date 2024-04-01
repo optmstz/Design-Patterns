b82daf1f62ba4738a33fa4f69f40724d7fd91dc8
Viktor Yelenetskyi <ipz223_yevv@student.ztu.edu.ua>
2024-04-01 19:38:02 +0300

    Created Reporting Service with interface

---
+++ lab-01/Programing-principles/ClassLibrary/Reporting/IReporter.cs
@@ -0,0 +1,14 @@
+﻿using ClassLibrary.WarehousePart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.ReportingPart
+{
+    public interface IReporter
+    {
+        void Report();
+    }
+}
---
+++ lab-01/Programing-principles/ClassLibrary/Reporting/ReportingService.cs
@@ -0,0 +1,25 @@
+﻿using ClassLibrary.WarehousePart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary.ReportingPart.Reporters;
+
+namespace ClassLibrary.ReportingPart
+{
+    public class ReportingService
+    {
+        private readonly Warehouse _warehouse;
+
+        public ReportingService(Warehouse warehouse)
+        {
+            _warehouse = warehouse;
+        }
+
+        public void RegisterReport(IReporter reporter)
+        {
+            reporter.Report();
+        }
+    }
+}

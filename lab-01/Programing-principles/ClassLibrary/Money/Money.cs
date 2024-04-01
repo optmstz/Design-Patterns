49fcdd60bf78777ee84ec4e9984d1630bee302f9
Viktor Yelenetskyi <ipz223_yevv@student.ztu.edu.ua>
2024-04-01 19:31:07 +0300

    Created Money class, have two prop. wholePart and fractionalPart

---
+++ lab-01/Programing-principles/ClassLibrary/Money/Money.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary.MoneyPart
+{
+    public class Money : IMoney
+    {
+        public int wholePart { get; set; }
+        public int fractionalPart { get;  set; }
+
+
+        public Money(int wholePart, int fractionalPart)
+        {
+            this.wholePart = wholePart + fractionalPart / 100;
+            this.fractionalPart = fractionalPart % 100;
+        }
+
+        public virtual string DisplayAmount()
+        {
+            return $"{wholePart}.{fractionalPart}";
+        }
+    }
+}

31210f69f85bd01dffb36ba1785e32a0625cfeb6
Viktor Yelenetskyi <ipz223_yevv@student.ztu.edu.ua>
2024-04-01 19:34:17 +0300

    Added interface which display reducing of product price, realized in ProductManager

---
+++ lab-01/Programing-principles/ClassLibrary/Product/Manager/IProductManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary.MoneyPart;
+
+namespace ClassLibrary.ProductPart.Manager
+{
+    public interface IProductManager
+    {
+        void ReduceProductPrice(Money reduceValue);
+    }
+}
---
+++ lab-01/Programing-principles/ClassLibrary/Product/Manager/ProductManager.cs
@@ -0,0 +1,28 @@
+﻿using ClassLibrary.MoneyPart;
+
+namespace ClassLibrary.ProductPart.Manager
+{
+    public class ProductManager
+    {
+
+        public void ReduceProductPrice(IProduct productInfo, Money reductionAmount)
+        {
+            if (productInfo.price.wholePart > reductionAmount.wholePart ||
+                productInfo.price.wholePart == reductionAmount.wholePart && productInfo.price.fractionalPart >= reductionAmount.fractionalPart)
+            {
+                productInfo.price.wholePart -= reductionAmount.wholePart;
+                productInfo.price.fractionalPart -= reductionAmount.fractionalPart;
+
+                if (productInfo.price.fractionalPart < 0)
+                {
+                    productInfo.price.wholePart--;
+                    productInfo.price.fractionalPart += 100;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error: reduce price more than current one.");
+            }
+        }
+    }
+}

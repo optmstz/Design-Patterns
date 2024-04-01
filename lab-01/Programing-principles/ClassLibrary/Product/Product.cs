3795401de6f5ba00a44eca4acdb9c6a899ae15f0
Viktor Yelenetskyi <ipz223_yevv@student.ztu.edu.ua>
2024-04-01 19:33:12 +0300

    Added realization of Product class and interface

---
+++ lab-01/Programing-principles/ClassLibrary/Product/Product.cs
@@ -0,0 +1,20 @@
+﻿using ClassLibrary.MoneyPart;
+
+namespace ClassLibrary.ProductPart
+{
+    public class Product : IProduct
+    {
+        public string name { get; protected set; }
+        public string category { get; protected set; }
+
+        public Money price { get; protected set; }
+
+        public Product(string name, string category, Money price)
+        {
+            this.name = name;
+            this.category = category;
+            this.price = price;
+        }
+
+    }
+}

b71178a04ab24be45cb4f38ac27cffa8309107bf
Viktor Yelenetskyi <ipz223_yevv@student.ztu.edu.ua>
2024-05-19 15:50:59 +0300

    Created factory interface

---
+++ lab-02/AbstractFactory/AbstractFactoryClassLibrary/Factory/IDeviceFactory.cs
@@ -0,0 +1,17 @@
+﻿using AbstractFactoryClassLibrary.Devices.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactoryClassLibrary.Factory
+{
+    public interface IDeviceFactory
+    {
+        ILaptop CreateLaptop();
+        INetbook CreateNetbook();
+        IEBook CreateEBook();
+        ISmartphone CreateSmartphone();
+    }
+}

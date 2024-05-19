242cb4044f8b28f1e83ea77065ffec963a9c3445
Viktor Yelenetskyi <ipz223_yevv@student.ztu.edu.ua>
2024-05-19 15:04:02 +0300

    Added ManagerCall purchaser

---
+++ lab-02/Generating-patterns/Generating-patterns-class-library/Purchasers/ManagerCall.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generating_patterns_class_library.Purchasers
+{
+    public class ManagerCall
+    {
+        private readonly ISubscriptionFactory _factory;
+
+        public ManagerCall(ISubscriptionFactory factory)
+        {
+            _factory = factory;
+        }
+
+        public ISubscription PurchaseSubscription(string managerCode)
+        {
+            if (ConfirmWithManager(managerCode))
+            {
+                Console.WriteLine("Purchasing subscription through Manager Call");
+                return _factory.CreateSubscription();
+            }
+            else
+            {
+                Console.WriteLine("Manager confirmation failed.");
+                return null!;
+            }
+        }
+
+        private bool ConfirmWithManager(string managerCode)
+        {
+            return managerCode == "CONFIRMED";
+        }
+    }
+}

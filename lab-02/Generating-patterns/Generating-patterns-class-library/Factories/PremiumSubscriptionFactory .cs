65aea656c2b5a76aec2e602a24b1c3be252afc41
Viktor Yelenetskyi <ipz223_yevv@student.ztu.edu.ua>
2024-05-19 15:03:05 +0300

    Created concrete factories for each Subscription

---
+++ lab-02/Generating-patterns/Generating-patterns-class-library/Factories/PremiumSubscriptionFactory .cs
@@ -0,0 +1,31 @@
+﻿using Generating_patterns_class_library.Subscriptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generating_patterns_class_library.Factories
+{
+    public class PremiumSubscriptionFactory : ISubscriptionFactory
+    {
+        private readonly decimal _monthlyPrice;
+        private readonly int _minimalSubscriptionPeriod;
+        private readonly List<string> _includedChannels;
+        private readonly bool _accessToPremiumContent;
+
+        public PremiumSubscriptionFactory(decimal monthlyPrice, int minimalSubscriptionPeriod,
+            List<string> includedChannels, bool accessToPremiumContent)
+        {
+            _monthlyPrice = monthlyPrice;
+            _minimalSubscriptionPeriod = minimalSubscriptionPeriod;
+            _includedChannels = includedChannels;
+            _accessToPremiumContent = accessToPremiumContent;
+        }
+
+        public ISubscription CreateSubscription()
+        {
+            return new PremiumSubscription(_monthlyPrice, _minimalSubscriptionPeriod, _includedChannels, _accessToPremiumContent);
+        }
+    }
+}

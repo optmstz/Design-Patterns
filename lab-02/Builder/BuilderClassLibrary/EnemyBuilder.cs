2771a51f7dbeb4457fee7e02c485ded67e460de8
Viktor Yelenetskyi <ipz223_yevv@student.ztu.edu.ua>
2024-05-19 16:53:28 +0300

    Realizations of builder interface

---
+++ lab-02/Builder/BuilderClassLibrary/EnemyBuilder.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BuilderClassLibrary
+{
+    public class EnemyBuilder : ICharacterBuilder
+    {
+        private Enemy _enemy = new Enemy();
+
+        public ICharacterBuilder SetHeight(double height)
+        {
+            _enemy.Height = height;
+            return this;
+        }
+
+        public ICharacterBuilder SetBuild(string build)
+        {
+            _enemy.Build = build;
+            return this;
+        }
+
+        public ICharacterBuilder SetHairColor(string hairColor)
+        {
+            _enemy.HairColor = hairColor;
+            return this;
+        }
+
+        public ICharacterBuilder SetEyeColor(string eyeColor)
+        {
+            _enemy.EyeColor = eyeColor;
+            return this;
+        }
+
+        public ICharacterBuilder SetClothing(string clothing)
+        {
+            _enemy.Clothing = clothing;
+            return this;
+        }
+
+        public ICharacterBuilder SetInventory(List<string> inventory)
+        {
+            _enemy.Inventory = inventory;
+            return this;
+        }
+
+        public ICharacterBuilder AddGoodDeed(string deed)
+        {
+            throw new InvalidOperationException("This method is not applicable for EnemyBuilder");
+        }
+
+        public ICharacterBuilder AddEvilDeed(string deed)
+        {
+            if (_enemy.EvilDeeds == null)
+            {
+                _enemy.EvilDeeds = new List<string> { deed };
+            }
+            else
+            {
+                _enemy.EvilDeeds.Add(deed);
+            }
+            return this;
+        }
+
+        public ICharacter Build()
+        {
+            return _enemy;
+        }
+    }
+}
---
+++ lab-02/Builder/BuilderClassLibrary/HeroBuilder.cs
@@ -0,0 +1,58 @@
+﻿using BuilderClassLibrary;
+
+public class HeroBuilder : ICharacterBuilder
+{
+    private Hero _hero = new Hero();
+
+    public ICharacterBuilder SetHeight(double height)
+    {
+        _hero.Height = height;
+        return this;
+    }
+
+    public ICharacterBuilder SetBuild(string build)
+    {
+        _hero.Build = build;
+        return this;
+    }
+
+    public ICharacterBuilder SetHairColor(string hairColor)
+    {
+        _hero.HairColor = hairColor;
+        return this;
+    }
+
+    public ICharacterBuilder SetEyeColor(string eyeColor)
+    {
+        _hero.EyeColor = eyeColor;
+        return this;
+    }
+
+    public ICharacterBuilder SetClothing(string clothing)
+    {
+        _hero.Clothing = clothing;
+        return this;
+    }
+
+    public ICharacterBuilder SetInventory(List<string> inventory)
+    {
+        _hero.Inventory = inventory.ToList();
+        return this;
+    }
+
+    public ICharacterBuilder AddGoodDeed(string deed)
+    {
+        _hero.GoodDeeds.Add(deed);
+        return this;
+    }
+
+    public ICharacterBuilder AddEvilDeed(string deed)
+    {
+        throw new InvalidOperationException("This method is not applicable for HeroBuilder");
+    }
+
+    public ICharacter Build()
+    {
+        return _hero;
+    }
+}

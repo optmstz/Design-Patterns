36c361ca86c155faa01694b70aecec6e27f6c5d6
Viktor Yelenetskyi <ipz223_yevv@student.ztu.edu.ua>
2024-05-19 15:02:44 +0300

    Created interfece for factories

---
+++ lab-02/Generating-patterns/Generating-patterns-class-library/ISubscriptionFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generating_patterns_class_library
+{
+    public interface ISubscriptionFactory
+    {
+        ISubscription CreateSubscription();
+    }
+}

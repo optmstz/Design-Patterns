23a63e8e62ba87546b051b253875063a6aa75d5f
Viktor Yelenetskyi <ipz223_yevv@student.ztu.edu.ua>
2024-05-19 15:03:47 +0300

    Added WebSite purchaser

---
+++ lab-02/Generating-patterns/Generating-patterns-class-library/Purchasers/WebSite.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generating_patterns_class_library.Purchasers
+{
+    public class WebSite
+    {
+        private readonly ISubscriptionFactory _factory;
+
+        public WebSite(ISubscriptionFactory factory)
+        {
+            _factory = factory;
+        }
+
+        public ISubscription PurchaseSubscription(string userEmail)
+        {
+            if (ValidateUserEmail(userEmail))
+            {
+                Console.WriteLine("Purchasing subscription through Website");
+                return _factory.CreateSubscription();
+            }
+            else
+            {
+                Console.WriteLine("Invalid email address provided.");
+                return null!;
+            }
+        }
+
+        private bool ValidateUserEmail(string email)
+        {
+            return email.Contains("@") && email.Contains(".");
+        }
+    }
+}

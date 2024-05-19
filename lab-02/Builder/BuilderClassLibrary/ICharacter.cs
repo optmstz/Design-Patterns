78a516958a557f5b22a916b47cd9f66ba2424519
Viktor Yelenetskyi <ipz223_yevv@student.ztu.edu.ua>
2024-05-19 16:51:45 +0300

    Created a character interface

---
+++ lab-02/Builder/BuilderClassLibrary/ICharacter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderClassLibrary
+{
+    public interface ICharacter
+    {
+        double Height { get; set; }
+        string Build { get; set; }
+        string HairColor { get; set; }
+        string EyeColor { get; set; }
+        string Clothing { get; set; }
+        List<string> Inventory { get; set; }
+    }
+}

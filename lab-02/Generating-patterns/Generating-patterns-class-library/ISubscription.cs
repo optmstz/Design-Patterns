dfae14905a0207ea87c1d8f69aab28b264e5e4c1
Viktor Yelenetskyi <ipz223_yevv@student.ztu.edu.ua>
2024-05-19 14:26:30 +0300

    Implemented the ISubscription interface

---
+++ lab-02/Generating-patterns/Generating-patterns-class-library/ISubscription.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generating_patterns_class_library
+{
+    internal interface ISubscription
+    {
+        decimal MonthlyPrice { get; set; }
+        int MinimalSubscriptionPeriod { get; set; }
+        List<string> IncludedChannels { get; set; }
+        bool AccessToPremiumContent { get; }
+    }
+}

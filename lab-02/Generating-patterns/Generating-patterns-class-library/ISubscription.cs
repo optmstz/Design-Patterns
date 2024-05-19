0dace105e97669300f1d0d8971bbf0dac368392b
Viktor Yelenetskyi <ipz223_yevv@student.ztu.edu.ua>
2024-05-19 15:02:13 +0300

    Changed access modifier from internal to public

--- lab-02/Generating-patterns/Generating-patterns-class-library/ISubscription.cs
+++ lab-02/Generating-patterns/Generating-patterns-class-library/ISubscription.cs
@@ -6,11 +6,12 @@ using System.Threading.Tasks;
 
 namespace Generating_patterns_class_library
 {
-    internal interface ISubscription
+    public interface ISubscription
     {
         decimal MonthlyPrice { get; }
         int MinimalSubscriptionPeriod { get; }
         List<string> IncludedChannels { get; }
         bool AccessToPremiumContent { get; }
+        decimal GetPrice();
     }
 }

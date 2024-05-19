c1b1998517817a8c4b10bd828b5017e0ed307693
Viktor Yelenetskyi <ipz223_yevv@student.ztu.edu.ua>
2024-05-19 16:53:14 +0300

    Builder Interface

---
+++ lab-02/Builder/BuilderClassLibrary/ICharacterBuilder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderClassLibrary
+{
+    public interface ICharacterBuilder
+    {
+        ICharacterBuilder SetHeight(double height);
+        ICharacterBuilder SetBuild(string build);
+        ICharacterBuilder SetHairColor(string hairColor);
+        ICharacterBuilder SetEyeColor(string eyeColor);
+        ICharacterBuilder SetClothing(string clothing);
+        ICharacterBuilder SetInventory(List<string> inventory);
+        ICharacterBuilder AddGoodDeed(string deed);
+        ICharacterBuilder AddEvilDeed(string deed);
+        ICharacter Build();
+    }
+}

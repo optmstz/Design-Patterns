c9b6aabbe366b7f26a3be991c67f9b7539c42e6b
Viktor Yelenetskyi <ipz223_yevv@student.ztu.edu.ua>
2024-05-19 16:01:00 +0300

    Created a Authenticator with singleton pattern

--- lab-02/Singleton/Singleton/Program.cs
+++ lab-02/Singleton/Singleton/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SingletonClassLibrary;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Authenticator authenticator1 = Authenticator.GetInstance();
+        authenticator1.AuthenticateUser("user1", "password1");
+
+        Authenticator authenticator2 = Authenticator.GetInstance();
+        authenticator2.AuthenticateUser("user2", "password2");
+
+        Console.WriteLine("authenticator1 == authenticator2: " + (authenticator1 == authenticator2));
+    }
+}
---
+++ lab-02/Singleton/SingletonClassLibrary/Authenticator.cs
@@ -0,0 +1,35 @@
+﻿namespace SingletonClassLibrary
+{
+    public class Authenticator
+    {
+        private static Authenticator? instance;
+
+        private static readonly object lockObject = new object();
+
+        private Authenticator()
+        {
+            Console.WriteLine("Initializing Singleton");
+        }
+
+        public static Authenticator GetInstance()
+        {
+            if (instance == null)
+            {
+                lock (lockObject)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Authenticator();
+                    }
+                }
+            }
+
+            return instance;
+        }
+
+        public void AuthenticateUser(string username, string password)
+        {
+            Console.WriteLine("Authenticating user: " + username);
+        }
+    }
+}

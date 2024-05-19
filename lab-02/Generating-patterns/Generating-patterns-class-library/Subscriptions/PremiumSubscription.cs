f43bbc7d99c4079b621bcde5c1545f1fbb777a2a
Viktor Yelenetskyi <ipz223_yevv@student.ztu.edu.ua>
2024-05-19 15:01:26 +0300

    Some changes in concrete Subscriptions

---
+++ lab-02/Generating-patterns/Generating-patterns-class-library/Subscriptions/PremiumSubscription.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generating_patterns_class_library.Subscriptions
+{
+    public class PremiumSubscription : ISubscription
+    {
+        public decimal MonthlyPrice { get; private set; }
+        public int MinimalSubscriptionPeriod { get; private set; }
+        public List<string> IncludedChannels { get; private set; }
+        public bool AccessToPremiumContent { get; private set; }
+
+        public PremiumSubscription(decimal monthlyPrice, int minimalSubscriptionPeriod,
+            List<string> includedChannels, bool accessToPremiumContent)
+        {
+            MonthlyPrice = monthlyPrice;
+            MinimalSubscriptionPeriod = minimalSubscriptionPeriod;
+            IncludedChannels = includedChannels;
+            AccessToPremiumContent = accessToPremiumContent;
+        }
+
+        public decimal GetPrice()
+        {
+            return MonthlyPrice;
+        }
+    }
+}

ab7524a80e55feef0ccebe2874552ba1d04a92d7
Viktor Yelenetskyi <ipz223_yevv@student.ztu.edu.ua>
2024-05-19 16:53:02 +0300

    To previous commit

---
+++ lab-02/Builder/BuilderClassLibrary/Hero.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BuilderClassLibrary
+{
+    public class Hero : ICharacter
+    {
+        public double Height { get; set; }
+        public string Build { get; set; } = string.Empty;
+        public string HairColor { get; set; } = string.Empty;
+        public string EyeColor { get; set; } = string.Empty;
+        public string Clothing { get; set; } = string.Empty;
+        public List<string> Inventory { get; set; }
+        public List<string> GoodDeeds { get; set; }
+
+        public Hero()
+        {
+            Inventory = new List<string>();
+            GoodDeeds = new List<string>();
+        }
+
+        public override string ToString()
+        {
+            string inventoryStr = string.Join(", ", Inventory);
+            string goodDeedsStr = string.Join(", ", GoodDeeds);
+
+            return $"Hero: Height - {Height}, Build - {Build}, Hair Color - {HairColor}, Eye Color - {EyeColor}, Clothing - {Clothing}, Inventory - [{inventoryStr}], Good Deeds - [{goodDeedsStr}]";
+        }
+    }
+}

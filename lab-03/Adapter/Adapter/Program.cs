28b2ea2ad64e3f39628cd812eec899674c990176
Viktor Yelenetskyi <ipz223_yevv@student.ztu.edu.ua>
2024-05-19 18:02:12 +0300

    Adapter

---
+++ lab-03/Adapter/Adapter/Program.cs
@@ -0,0 +1,17 @@
+﻿using AdapterClassLibrary;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        FileLoggerAdapter fileLogger = new FileLoggerAdapter();
+
+        fileLogger.Log("Information message");
+        fileLogger.Error("Error message");
+        fileLogger.Warn("Warning message");
+
+        fileLogger.LogToFile("Logging to file");
+        fileLogger.ErrorToFile("Error message");
+        fileLogger.WarnToFile("Warning message");
+    }
+}
---
+++ lab-03/Adapter/AdapterClassLibrary/FileLoggerAdapter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterClassLibrary
+{
+    public class FileLoggerAdapter : Logger
+    {
+        private FileWriter fileWriter;
+
+        public FileLoggerAdapter()
+        {
+            this.fileWriter = new FileWriter();
+        }
+
+        public void LogToFile(string message)
+        {
+            fileWriter.Write(message);
+        }
+
+        public void ErrorToFile(string message)
+        {
+            fileWriter.Write("ERROR: " + message);
+        }
+
+        public void WarnToFile(string message)
+        {
+            fileWriter.Write("WARNING: " + message);
+        }
+    }
+}

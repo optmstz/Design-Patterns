17a5240082cb0499b431606468833c7fdf6f7d45
Viktor Yelenetskyi <ipz223_yevv@student.ztu.edu.ua>
2024-05-19 15:04:13 +0300

    Added MobileApp purchaser

---
+++ lab-02/Generating-patterns/Generating-patterns-class-library/Purchasers/MobileApp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generating_patterns_class_library.Purchasers
+{
+    public class MobileApp
+    {
+        private readonly ISubscriptionFactory _factory;
+
+        public MobileApp(ISubscriptionFactory factory)
+        {
+            _factory = factory;
+        }
+
+        public ISubscription PurchaseSubscription(string userToken)
+        {
+            if (AuthenticateUser(userToken))
+            {
+                Console.WriteLine("Purchasing subscription through Mobile App");
+                return _factory.CreateSubscription();
+            }
+            else
+            {
+                Console.WriteLine("User authentication failed.");
+                return null!;
+            }
+        }
+
+        private bool AuthenticateUser(string token)
+        {
+            // Простий приклад автентифікації користувача
+            return !string.IsNullOrEmpty(token);
+        }
+    }
+}

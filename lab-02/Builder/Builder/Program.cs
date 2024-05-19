7e9caccb5bea29f38e479ae55aeecd4035a39419
Viktor Yelenetskyi <ipz223_yevv@student.ztu.edu.ua>
2024-05-19 16:52:25 +0300

    Create Enemy and Hero classes which implements the interfece ICharacterBuilder

--- lab-02/Builder/Builder/Program.cs
+++ lab-02/Builder/Builder/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection.Emit;
+
+namespace BuilderClassLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating a hero
+            var heroBuilder = new HeroBuilder();
+            var heroDirector = new Director(heroBuilder);
+
+            heroDirector.ConstructHero(
+                height: 180,
+                build: "Muscular",
+                hairColor: "Brown",
+                eyeColor: "Green",
+                clothing: "Armor",
+                inventory: new List<string> { "Sword", "Shield" },
+                goodDeeds: new List<string> { "Saved a village", "Rescued a princess" }
+            );
+
+            var hero = heroBuilder.Build();
+            Console.WriteLine("Hero created:");
+            Console.WriteLine(hero);
+
+            Console.WriteLine();
+
+            // Creating an enemy
+            var enemyBuilder = new EnemyBuilder();
+            var enemyDirector = new Director(enemyBuilder);
+
+            enemyDirector.ConstructEnemy(
+                height: 170,
+                build: "Slender",
+                hairColor: "Black",
+                eyeColor: "Red",
+                clothing: "Dark Robe",
+                inventory: new List<string> { "Dark Magic Tome", "Dagger" },
+                evilDeeds: new List<string> { "Destroyed a kingdom", "Enslaved innocent creatures" }
+            );
+
+            var enemy = enemyBuilder.Build();
+            Console.WriteLine("Enemy created:");
+            Console.WriteLine(enemy);
+        }
+    }
+}
---
+++ lab-02/Builder/BuilderClassLibrary/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace BuilderClassLibrary
+{
+    public class Enemy : ICharacter
+    {
+        public double Height { get; set; }
+        public string Build { get; set; } = string.Empty;
+        public string HairColor { get; set; } = string.Empty;
+        public string EyeColor { get; set; } = string.Empty;
+        public string Clothing { get; set; } = string.Empty;
+        public List<string> Inventory { get; set; }
+        public List<string> EvilDeeds { get; set; }
+
+        public Enemy()
+        {
+            Inventory = new List<string>();
+            EvilDeeds = new List<string>();
+        }
+
+        public override string ToString()
+        {
+            string inventoryStr = string.Join(", ", Inventory);
+            string evilDeedsStr = string.Join(", ", EvilDeeds);
+
+            return $"Enemy: Height - {Height}, Build - {Build}, Hair Color - {HairColor}, Eye Color - {EyeColor}, Clothing - {Clothing}, Inventory - [{inventoryStr}], Evil Deeds - [{evilDeedsStr}]";
+        }
+    }
+}

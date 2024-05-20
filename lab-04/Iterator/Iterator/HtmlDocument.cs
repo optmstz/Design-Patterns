37968d5c1759dfe511cb6b28eaff772ea6b5abf2
Viktor Yelenetskyi <ipz223_yevv@student.ztu.edu.ua>
2024-05-20 11:47:39 +0300

    Added iterator pattern

---
+++ lab-04/Iterator/Iterator/HtmlDocument.cs
@@ -0,0 +1,26 @@
+﻿using ComposerClassLibrary;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Iterator
+{
+    public class HtmlDocument : IEnumerable<LightNode>
+    {
+        private LightNode _rootNode;
+
+        public HtmlDocument(LightNode rootNode)
+        {
+            _rootNode = rootNode;
+        }
+
+        public IEnumerator<LightNode> GetEnumerator()
+        {
+            return new HtmlDepthFirstIterator(_rootNode);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}

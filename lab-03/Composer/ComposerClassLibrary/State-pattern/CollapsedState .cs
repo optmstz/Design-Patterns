92399378ce33d21747e2d275e6d5c499b3496576
Viktor Yelenetskyi <ipz223_yevv@student.ztu.edu.ua>
2024-05-20 12:27:43 +0300

    Added State pattern, we have ExpandedState and CollapseState and also added method fo them

---
+++ lab-03/Composer/ComposerClassLibrary/State-pattern/CollapsedState .cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComposerClassLibrary.State_pattern
+{
+    public class CollapsedState : ElementState
+    {
+        public override void Display()
+        {
+            Console.WriteLine($"Displaying {_element?.GetType().Name} in collapsed state.");
+        }
+
+        public override void Collapse()
+        {
+            Console.WriteLine($"Collapsing {_element?.GetType().Name} in collapsed state.");
+        }
+    }
+}
---
+++ lab-03/Composer/ComposerClassLibrary/State-pattern/ElementState.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComposerClassLibrary.State_pattern
+{
+    public abstract class ElementState
+    {
+        protected LightElementNode? _element;
+
+        public void SetElement(LightElementNode element)
+        {
+            this._element = element;
+        }
+
+        public virtual void Display()
+        {
+            Console.WriteLine($"Displaying {_element?.GetType().Name} in default state.");
+        }
+
+        public virtual void Collapse()
+        {
+            Console.WriteLine($"Collapsing {_element?.GetType().Name} in default state.");
+        }
+    }
+}

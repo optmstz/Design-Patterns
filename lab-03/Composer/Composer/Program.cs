95ef79717a5d1306c37fbf77297a28137911f1d0
Viktor Yelenetskyi <ipz223_yevv@student.ztu.edu.ua>
2024-05-20 12:06:31 +0300

    Added Command pattern

--- lab-03/Composer/Composer/Program.cs
+++ lab-03/Composer/Composer/Program.cs
@@ -1,30 +1,37 @@
 ﻿using ComposerClassLibrary;
+using ComposerClassLibrary.Command;
 
 class Program
 {
     static void Main(string[] args)
     {
+        // Creating commands
+        var div = new LightElementNode("div", true, false);
+        var addDivClassCommand = new AddClassCommand(div, "container");
 
-        LightElementNode div = new LightElementNode("div", true, false);
-        div.AddClass("container");
+        var h1 = new LightElementNode("h1", true, false);
+        var h1Text = new LightTextNode("Welcome to LightHTML!");
+        var addH1ChildCommand = new AddChildCommand(h1, h1Text);
 
-        LightElementNode h1 = new LightElementNode("h1", true, false);
-        LightTextNode h1Text = new LightTextNode("Welcome to LightHTML!");
-        h1.AddChild(h1Text);
+        var p = new LightElementNode("p", false, false);
+        var pText = new LightTextNode("This is a simple example of LightHTML markup language.");
+        var addPChildCommand = new AddChildCommand(p, pText);
 
-        LightElementNode p = new LightElementNode("p", false, false);
-        LightTextNode pText = new LightTextNode("This is a simple example of LightHTML markup language.");
-        p.AddChild(pText);
+        var br = new LightElementNode("br", false, true);
 
-        LightElementNode br = new LightElementNode("br", false, true);
-        Console.WriteLine("Break tag:");
-        Console.WriteLine(br.OuterHTML());
-        Console.WriteLine();
+        var invoker = new CommandInvoker();
+        invoker.AddCommand(addDivClassCommand);
+        invoker.AddCommand(addH1ChildCommand);
+        invoker.AddCommand(addPChildCommand);
 
+        // Execute commands
+        invoker.ExecuteCommands();
 
+        // Adding child to div
         div.AddChild(h1);
         div.AddChild(p);
 
+        // Printing HTML
         Console.WriteLine("Outer HTML:");
         Console.WriteLine(div.OuterHTML());
         Console.WriteLine();
@@ -32,5 +39,4 @@ class Program
         Console.WriteLine("Inner HTML:");
         Console.WriteLine(div.InnerHTML());
     }
-
 }
